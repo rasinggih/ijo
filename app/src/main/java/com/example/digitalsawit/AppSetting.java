4d333b8d2a419a923391a674435098489f35d3f7
Awalmlna <awal.maulana@gmail.com>
2021-04-14 16:34:07 +0700

    front end form app setting

--- app/src/main/java/com/example/digitalsawit/AppSetting.java
+++ app/src/main/java/com/example/digitalsawit/AppSetting.java
@@ -1,14 +1,53 @@
 package com.example.digitalsawit;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class AppSetting extends AppCompatActivity {
 
+    DatabaseHelper dbhelper;
+
+    AutoCompleteTextView acAppLanguage;
+    ImageView profilePicture, dropAppLanguage, imgInsertLogo;
+    TextView tvBgImg;
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app_setting);
+
+        dbhelper = new DatabaseHelper(this);
+
+        acAppLanguage = findViewById(R.id.acAppLanguage);
+        dropAppLanguage = findViewById(R.id.dropAppLanguage);
+        imgInsertLogo = findViewById(R.id.imgInsertLogo);
+        tvBgImg = findViewById(R.id.tvBackgroundImg);
+
+        try {
+            Bitmap compressedBitmap = BitmapFactory.decodeByteArray(dbhelper.get_gambar_user(), 0, dbhelper.get_gambar_user().length);
+            profilePicture.setImageBitmap(compressedBitmap);
+            profilePicture.setVisibility(View.VISIBLE);
+            profilePicture.setBackground(null);
+            profilePicture.setForeground(null);
+            profilePicture.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
